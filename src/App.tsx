78c80d074e26a3f44f6c319f8566f5f40031cde9
Shubham-Yadav22 <432shobhit@gmail.com>
2025-12-12 14:54:20 +0530

    updated the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { lazy, Suspense } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/ui-components/ui/toaster";
+import { Toaster as Sonner } from "@/ui-components/ui/sonner";
+import { TooltipProvider } from "@/ui-components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { ErrorBoundary } from "@/components/common/ErrorBoundary";
-import { CarouselSkeleton } from "@/components/ui/loading-skeleton";
-import { Layout } from "@/components/layout/Layout";
+import { ErrorBoundary } from "@/Component/common/errorBoundary";
+import { CarouselSkeleton } from "@/ui-components/ui/loading-skeleton";
+import { Layout } from "@/Component/layout/layout";
 
 // Lazy load pages for code splitting
-const Home = lazy(() => import("./pages/Home"));
-const Search = lazy(() => import("./pages/Search"));
-const ShowDetail = lazy(() => import("./pages/ShowDetail"));
-const NotFound = lazy(() => import("./pages/NotFound"));
+const Home = lazy(() => import("./Component/home/home"));
+const Search = lazy(() => import("./Component/search/search"));
+const ShowDetail = lazy(() => import("./Component/showDetail/showdetail"));
+const NotFound = lazy(() => import("./Component/notFound/notFound"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
